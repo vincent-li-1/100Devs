bc7bd14fd4b5e0de78b99500d652507d84e49c94
Vincent Li <vincentfli.1017@gmail.com>
2023-05-20 10:30:07 -0500

    forgot to save file

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,6 +1,6 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
-function MakeSkaterCharacter(brand, color, height, age) {
-	this.boardBrand = board;
+function MakeSkaterCharacter(name, color, height, age) {
+	this.name = name;
 	this.boardColor = color;
 	this.skaterHeight = height;
 	this.skaterAge = age;
@@ -14,3 +14,8 @@ function MakeSkaterCharacter(brand, color, height, age) {
 		console.log('ouch!');
 	}
 }
+
+let tony = new MakeSkaterCharacter('Tony Hawk', 'blue', 72, 50);
+console.log(tony);
+tony.ageUp();
+console.log(tony.skaterAge);
